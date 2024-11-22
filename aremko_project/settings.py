c2ec362911ae0672e4a08eb93113e549c4522e5c
simon-aguilera <saguilera1608@gmail.com>
2024-11-22 00:20:34 -0300

    Update settings.py

--- aremko_project/settings.py
+++ aremko_project/settings.py
@@ -86,9 +86,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'ventas' / 'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
