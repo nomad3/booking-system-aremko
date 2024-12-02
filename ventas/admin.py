f15e61a1c9f93649f7a884ee5475c3f4654c8bfb
simon-aguilera <saguilera1608@gmail.com>
2024-12-02 17:34:25 -0300

    Revert "Update admin.py"
    This reverts commit 0c0ed9e0349a9e7a0b94009bab2338f9fb6f74a3.

--- ventas/admin.py
+++ ventas/admin.py
@@ -64,27 +64,6 @@ class ReservaServicioInline(admin.TabularInline):
     class Media:
         js = ('admin/js/reserva_servicio_inline.js',)  # Asegúrate de tener el archivo JS correcto
 
-# Primero definimos el formulario
-class ReservaProductoInlineForm(forms.ModelForm):
-    class Meta:
-        model = ReservaProducto
-        fields = ['producto', 'cantidad', 'precio_base', 'valor_total']
-        widgets = {
-            'producto': PrecioSelect(choices=[], attribute={}),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        productos = Producto.objects.order_by('nombre')
-        choices = [(producto.id, producto.nombre) for producto in productos]
-        precios = {str(producto.id): {'precio-base': producto.precio_base} for producto in productos}
-        self.fields['producto'].choices = choices
-        self.fields['producto'].widget = PrecioSelect(choices=choices, attribute=precios)
-        # Marcar los campos precio_base y valor_total como de solo lectura
-        self.fields['precio_base'].widget.attrs['readonly'] = True
-        self.fields['valor_total'].widget.attrs['readonly'] = True
-
-# Luego definimos el inline que usa el formulario
 class ReservaProductoInline(admin.TabularInline):
     model = ReservaProducto
     form = ReservaProductoInlineForm
@@ -99,7 +78,7 @@ class ReservaProductoInline(admin.TabularInline):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     class Media:
-        js = ('admin/js/reserva_producto_inline.js',)
+        js = ('admin/js/reserva_producto_inline.js',)  # Asegúrate de tener este archivo JS
 
 class PagoInline(admin.TabularInline):
     model = Pago
@@ -316,3 +295,22 @@ admin.site.register(VentaReserva, VentaReservaAdmin)
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Servicio, ServicioAdmin)
 admin.site.register(CategoriaServicio)
+
+class ReservaProductoInlineForm(forms.ModelForm):
+    class Meta:
+        model = ReservaProducto
+        fields = ['producto', 'cantidad', 'precio_base', 'valor_total']
+        widgets = {
+            'producto': PrecioSelect(choices=[], attribute={}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        productos = Producto.objects.order_by('nombre')
+        choices = [(producto.id, producto.nombre) for producto in productos]
+        precios = {str(producto.id): {'precio-base': producto.precio_base} for producto in productos}
+        self.fields['producto'].choices = choices
+        self.fields['producto'].widget = PrecioSelect(choices=choices, attribute=precios)
+        # Marcar los campos precio_base y valor_total como de solo lectura
+        self.fields['precio_base'].widget.attrs['readonly'] = True
+        self.fields['valor_total'].widget.attrs['readonly'] = True
