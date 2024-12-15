511c8ada57ca3c0944cfdada7d283b20f34579e7
ecolonco <ecolonco@gmail.com>
2024-12-14 21:27:10 -0300

    cambio total 2

--- ventas/admin.py
+++ ventas/admin.py
@@ -119,15 +119,16 @@ class VentaReservaAdmin(admin.ModelAdmin):
     
     # Guardar cambios con registro de movimiento
     def save_model(self, request, obj, form, change):
-        if not change:  # Si es una nueva instancia
-            MovimientoCliente.objects.create(
-                cliente=obj.cliente,
-                monto=obj.monto,
-                tipo_movimiento='Venta',
-                comentarios=f'Venta/Reserva #{obj.id}',  # Cambiado de 'descripcion' a 'comentarios'
-                usuario=request.user
-            )
+        if not obj.usuario:
+            obj.usuario = request.user
+        if change:
+            tipo = "Actualización de Venta/Reserva"
+            descripcion = f"Se ha actualizado la venta/reserva por {obj.total}"
+        else:
+            tipo = "Registro de Venta/Reserva"
+            descripcion = f"Se ha registrado una nueva venta/reserva por {obj.total}"
         super().save_model(request, obj, form, change)
+        registrar_movimiento(obj.cliente, tipo, descripcion, request.user)
 
     # Eliminar con registro de movimiento
     def delete_model(self, request, obj):
