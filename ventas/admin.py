55e05c480b9b66193c00331bf2d1359861c91016
simon-aguilera <saguilera1608@gmail.com>
2025-02-24 18:53:15 -0300

    usar jquery

--- ventas/admin.py
+++ ventas/admin.py
@@ -62,7 +62,10 @@ class ReservaServicioInline(admin.TabularInline):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     class Media:
-        js = ('ventas/js/reserva_servicio_admin.js',)  # Ruta relativa desde el directorio static
+        js = (
+            'admin/js/jquery.init.js',  # Necesario para django.jQuery
+            'ventas/js/reserva_servicio_admin.js',
+        )
 
 class ReservaProductoInline(admin.TabularInline):
     model = ReservaProducto
--- ventas/static/ventas/js/reserva_servicio_admin.js
+++ ventas/static/ventas/js/reserva_servicio_admin.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Actualizar slots al cambiar servicio
-    $(document).on('change', '.field-servicio select', function() {
-        var $form = $(this).closest('.dynamic-reservaservicio');
-        var servicioId = $(this).val();
+    // Usar django.jQuery en lugar de $
+    django.jQuery(document).on('change', '.field-servicio select', function() {
+        var $form = django.jQuery(this).closest('.dynamic-reservaservicio');
+        var servicioId = django.jQuery(this).val();
         var $horaInicio = $form.find('.field-hora_inicio select');
         
         if(servicioId) {
-            $.ajax({
+            django.jQuery.ajax({
                 url: '/admin/ventas/servicio/' + servicioId + '/slots/',
                 success: function(data) {
                     $horaInicio.empty();
-                    $.each(data.slots, function(index, value) {
-                        $horaInicio.append($('<option></option>').attr('value', value).text(value));
+                    django.jQuery.each(data.slots, function(index, value) {
+                        $horaInicio.append(django.jQuery('<option></option>').attr('value', value).text(value));
                     });
                 }
             });
