8935f33116efa03d6deda17e4b36ffb1938c8c59
simon-aguilera <saguilera1608@gmail.com>
2025-02-24 19:21:44 -0300

    Update reserva_servicio_admin.js

--- ventas/static/ventas/js/reserva_servicio_admin.js
+++ ventas/static/ventas/js/reserva_servicio_admin.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Usar django.jQuery en lugar de $
     django.jQuery(document).on('change', '.field-servicio select', function() {
         console.log('Servicio cambiado'); // Debug 1
-        var $form = django.jQuery(this).closest('.dynamic-reservaservicio');
+        var $row = django.jQuery(this).closest('.dynamic-reservaservicio');
         var servicioId = django.jQuery(this).val();
         console.log('ID Servicio:', servicioId); // Debug 2
         
@@ -11,19 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 url: '/admin/ventas/servicio/' + servicioId + '/slots/',
                 success: function(data) {
                     console.log('Respuesta API:', data); // Debug 3
-                    // Buscar por el ID que contiene 'hora_inicio'
-                    var $horaInicio = $form.find('select[id*="hora_inicio"]');
+                    // Selector por atributo name que termina con "-hora_inicio"
+                    var $horaInicio = $row.find('select[name$="-hora_inicio"]');
                     console.log('Elemento select encontrado:', $horaInicio.length); // Debug
                     
                     $horaInicio.empty();
                     django.jQuery.each(data.slots, function(index, value) {
                         console.log('Agregando slot:', value); // Debug 4
                         $horaInicio.append(
-                            django.jQuery('<option></option>')
-                                .attr('value', value)
-                                .text(value)
+                            django.jQuery('<option>', {
+                                value: value,
+                                text: value
+                            })
                         );
                     });
+                    
+                    // Si usas Select2
+                    if (django.jQuery.fn.select2) {
+                        $horaInicio.trigger('change.select2');
+                    }
                 },
                 error: function(xhr) {
                     console.error('Error en la solicitud:', xhr.statusText); // Debug 5
