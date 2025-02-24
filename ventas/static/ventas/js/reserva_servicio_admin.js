7f873727ec9b931bf8754f8a82f70c462d053728
simon-aguilera <saguilera1608@gmail.com>
2025-02-24 19:10:04 -0300

    Update reserva_servicio_admin.js

--- ventas/static/ventas/js/reserva_servicio_admin.js
+++ ventas/static/ventas/js/reserva_servicio_admin.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Usar django.jQuery en lugar de $
     django.jQuery(document).on('change', '.field-servicio select', function() {
+        console.log('Servicio cambiado'); // Debug 1
         var $form = django.jQuery(this).closest('.dynamic-reservaservicio');
         var servicioId = django.jQuery(this).val();
-        var $horaInicio = $form.find('.field-hora_inicio select');
+        console.log('ID Servicio:', servicioId); // Debug 2
         
         if(servicioId) {
             django.jQuery.ajax({
                 url: '/admin/ventas/servicio/' + servicioId + '/slots/',
                 success: function(data) {
+                    console.log('Respuesta API:', data); // Debug 3
+                    var $horaInicio = $form.find('.field-hora_inicio select');
                     $horaInicio.empty();
                     django.jQuery.each(data.slots, function(index, value) {
-                        $horaInicio.append(django.jQuery('<option></option>').attr('value', value).text(value));
+                        console.log('Agregando slot:', value); // Debug 4
+                        $horaInicio.append(
+                            django.jQuery('<option></option>')
+                                .attr('value', value)
+                                .text(value)
+                        );
                     });
+                },
+                error: function(xhr) {
+                    console.error('Error en la solicitud:', xhr.statusText); // Debug 5
                 }
             });
         }
