1f4e01e15efb4830d81e0e9db0cc07db9cf671f0
simon-aguilera <saguilera1608@gmail.com>
2025-04-25 14:14:04 -0400

    cambiar imagenes de home page desde admin

--- ventas/views/public_views.py
+++ ventas/views/public_views.py
@@ -15,11 +15,28 @@ def homepage_view(request):
 
     # --- Fetch Homepage Config ---
     hero_image_url = None
+    philosophy_image_url = None # Add variable for philosophy image
+    gallery_image_1_url = None  # Add variable for gallery image 1
+    gallery_image_2_url = None  # Add variable for gallery image 2
+    gallery_image_3_url = None  # Add variable for gallery image 3
+
     try:
         # HomepageConfig is a singleton, get the instance
         config = HomepageConfig.get_solo()
-        if config.hero_background_image: # Check if an image is set
+        # Get hero image URL
+        if config.hero_background_image:
             hero_image_url = config.hero_background_image.url
+        # Get philosophy image URL
+        if config.philosophy_image:
+            philosophy_image_url = config.philosophy_image.url
+        # Get gallery image URLs
+        if config.gallery_image_1:
+            gallery_image_1_url = config.gallery_image_1.url
+        if config.gallery_image_2:
+            gallery_image_2_url = config.gallery_image_2.url
+        if config.gallery_image_3:
+            gallery_image_3_url = config.gallery_image_3.url
+
     except HomepageConfig.DoesNotExist:
         # Handle case where the config hasn't been created yet
         pass
@@ -29,7 +46,11 @@ def homepage_view(request):
         'servicios': servicios,
         'categorias': categorias,
         'cart': cart,
-        'hero_image_url': hero_image_url, # Add the URL to the context
+        'hero_image_url': hero_image_url,
+        'philosophy_image_url': philosophy_image_url, # Add philosophy URL to context
+        'gallery_image_1_url': gallery_image_1_url,   # Add gallery 1 URL to context
+        'gallery_image_2_url': gallery_image_2_url,   # Add gallery 2 URL to context
+        'gallery_image_3_url': gallery_image_3_url,   # Add gallery 3 URL to context
     }
     return render(request, 'ventas/homepage.html', context)
 
